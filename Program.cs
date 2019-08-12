44c0a82aff035f8118c07c8e2ce2128c37d560fb
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-08-12 15:20:49 -0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pers_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
